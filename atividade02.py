be9efd094b323bd3cd95f419952144a5eff92b12
IsaacNascimento <isaacs.nascimento17@gmail.com.com>
2021-09-26 16:26:04 -0300

    primeiro commit

---
+++ atividade02.py
@@ -0,0 +1,9 @@
+!pip install plotly --upgrade #Baixa bibliotecas e funcionalidades, na qual não estão no ambiente colab. Plot para usar gráficos
+
+%reset -f  #Apagar as váriaveis do Phy para não herdar váriaveis anteriores
+
+import pandas as pd  #importar a biblioteca pandas como "pd", cuida de valores incosistentes
+
+import numpy as np #importar a biblioteca numpy as "np", para tramento de números
+
+import seaborn as sns #auxilio de gráficos
